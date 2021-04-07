a1562a43798c985102b58afaa51a613a63057ca8
Rafael S. Suguiura <rsato@technisys.com>
2021-04-07 20:27:26 -0300

    Added title

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h2>Fibonacci calculator</h2>
             <div>
                 <Route exact path="/" component={Fib} />
             </div>
